47d6f9fa0a2749ce6d9ae6bfda9ff1395ba3daca
Haoze Li <haozefrank@gmail.com>
2023-03-23 19:56:31 +1100

    Initial Commit

---
+++ Webdatacollect/WebdatacollectApp.swift
@@ -0,0 +1,17 @@
+//
+//  WebdatacollectApp.swift
+//  Webdatacollect
+//
+//  Created by haoze li on 3/23/23.
+//
+
+import SwiftUI
+
+@main
+struct WebdatacollectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
